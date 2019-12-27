86f6da0afdee5a19a3c3e9428336565d9894fef4
Anwesh43 <anweshthecool0@gmail.com>
2019-12-27 09:53:04 +0530

    adding StepBarView class

--- stepbarview/src/main/java/com/anwesh/uiprojects/stepbarview/StepBarView.kt
+++ stepbarview/src/main/java/com/anwesh/uiprojects/stepbarview/StepBarView.kt
@@ -53,3 +53,21 @@ fun Canvas.drawSBNode(i : Int, scale : Float, paint : Paint) {
     drawStepBars(scale, wSize, hSize, paint)
     restore()
 }
+
+class StepBarView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
