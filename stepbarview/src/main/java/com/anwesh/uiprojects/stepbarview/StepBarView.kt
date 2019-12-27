eda58c48210fd0389df65a106428c05849311115
Anwesh43 <anweshthecool0@gmail.com>
2019-12-27 09:44:16 +0530

    adding constants and libraries

--- stepbarview/src/main/java/com/anwesh/uiprojects/stepbarview/StepBarView.kt
+++ stepbarview/src/main/java/com/anwesh/uiprojects/stepbarview/StepBarView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.stepbarview
 /**
  * Created by anweshmishra on 27/12/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val bars : Int = 5
+val scGap : Float = 0.02f / bars
+val sizeFactor : Float = 2f
+val foreColor : Int = Color.parseColor("#0D47A1")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 30
