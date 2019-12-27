279a8a536e68cac3623483720a016bf98a5657e8
Anwesh43 <anweshthecool0@gmail.com>
2019-12-27 09:45:35 +0530

    adding Int and Float extension methods

--- stepbarview/src/main/java/com/anwesh/uiprojects/stepbarview/StepBarView.kt
+++ stepbarview/src/main/java/com/anwesh/uiprojects/stepbarview/StepBarView.kt
@@ -20,3 +20,8 @@ val sizeFactor : Float = 2f
 val foreColor : Int = Color.parseColor("#0D47A1")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 30
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
