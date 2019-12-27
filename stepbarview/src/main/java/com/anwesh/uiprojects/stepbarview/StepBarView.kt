7a4c6033230c9b2bf0d4885b37f0f985c775bd16
Anwesh43 <anweshthecool0@gmail.com>
2019-12-27 09:51:42 +0530

    adding Canvas extension methods

--- stepbarview/src/main/java/com/anwesh/uiprojects/stepbarview/StepBarView.kt
+++ stepbarview/src/main/java/com/anwesh/uiprojects/stepbarview/StepBarView.kt
@@ -25,3 +25,31 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawStepBar(i : Int, scale : Float, size : Float, gap : Float, paint : Paint) {
+    val sf : Float = scale.sinify().divideScale(i, bars)
+    save()
+    translate(i * gap, 0f)
+    drawRect(RectF(-gap / 2, -size * sf, gap / 2, 0f), paint)
+    restore()
+}
+
+fun Canvas.drawStepBars(scale : Float, wSize : Float, hSize : Float, paint : Paint) {
+    val gap : Float = wSize / bars
+    val hGap : Float = hSize / bars
+    for (j in 0..(bars - 1)) {
+        drawStepBar(j, scale, hGap * (j + 1), gap, paint)
+    }
+}
+
+fun Canvas.drawSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = foreColor
+    val wSize : Float = w / sizeFactor
+    val hSize : Float = h / (nodes + 1)
+    save()
+    translate(-wSize / 2, hSize * (i + 1))
+    drawStepBars(scale, wSize, hSize, paint)
+    restore()
+}
