99fd13156376648a4e729c5f6a971887c77a6095
Anwesh43 <anweshthecool0@gmail.com>
2019-12-27 10:12:32 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedstepbarview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedstepbarview/MainActivity.kt
@@ -2,11 +2,20 @@ package com.anwesh.uiprojects.linkedstepbarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.stepbarview.StepBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        StepBarView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
